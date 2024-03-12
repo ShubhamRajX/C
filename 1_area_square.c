97b0dab539643a92266df817ee9f38433e305b66
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-03-12 20:14:35 +0530

    tryToFindArea

---
+++ 1_area_square.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    // int side = 2;
+    // // int area = side*side;
+    // // printf("area is %d \n",area);
+    // printf("area is %d \n",side*side);
+    // return 0;
+
+    int side;
+    printf("Enter the value of side");
+    scanf("%d",&side);
+
+    // int area = side*side;
+    // printf("area of square is %d \n",area);
+
+    printf("area of square is %d \n",side*side);
+
+    return 0;
+}
