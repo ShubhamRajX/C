fa0e4c4ec9fb63a7f9f152d37c5d35eb408dff82
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-03-12 20:15:02 +0530

    tryToFindArea

---
+++ 2_areaofcircle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float radius;
+    printf("enter radius");
+    scanf("%df",&radius);
+
+    printf("area of circle is:%f \n",3.14*radius*radius);
+    return 0;
+}
