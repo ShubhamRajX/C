59e2fac5376b48f66d89c1fbe26104a4ff580998
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-03-13 20:18:55 +0530

    average

---
+++ 9_average.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+
+    printf("average : %d \n",((a+b+c)/3));
+
+    // int a = 5;
+    // int b = 6;
+    // (a<b)?printf("a less than b"):printf("a greater than b");
+    return 0;
+}
